eba8d83bbbd7149c56d6e26ded148ccf853e5fd7
tungduong150105 <tungduong150105@gmail.com>
2025-03-07 18:00:58 +0700

    Setup model User and Logi-api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate
+
+  rescue_from JWT::VerificationError, with: :invalid_token
+  rescue_from JWT::DecodeError, with: :decode_error
+
+  private
+
+  def authenticate
+    authorization_header = request.headers["Authorization"]
+    token = authorization_header.split(" ").last if authorization_header
+    decode_token = JsonWebToken.decode(token)
+  end
+
+  def invalid_token
+    render(json: {invalid_token: "Invalid Token"})
+  end
+
+  def decode_error
+    render(json: {decode_error: "Decode Error"})
+  end
 end
