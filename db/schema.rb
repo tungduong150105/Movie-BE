aede129698a7909c8f857faa7ff638865884b281
tungduong150105 <tungduong150105@gmail.com>
2025-03-08 09:16:51 +0700

    Change Sqlite3 to Postgres

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,14 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2025_03_07_101434) do
-  create_table("users", force: :cascade) do |t|
-    t.string("username")
-    t.string("password_digest")
-    t.datetime("created_at", null: false)
-    t.datetime("updated_at", null: false)
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
+
 end
